3138561749fd989778cabeba337d85f0ef84fb11
oisinredmond <oisinredmond@gmail.com>
2017-04-03 15:20:16 +0100

    Added gravity to player

--- OOPAssignment3/Assets/Scripts/FPSController.cs
+++ OOPAssignment3/Assets/Scripts/FPSController.cs
@@ -7,13 +7,13 @@ public class FPSController : MonoBehaviour {
 	public float speed = 2f;
 	public float sensitivity = 3f;
 	CharacterController player;
-	
 	public GameObject camera;
 	
 	private float moveForwardBack;
 	private float moveLeftRight;
 	private float rotationX;
 	private float rotationY;
+	private float vertVelocity;
 
 	void Start () 
 	{
@@ -28,14 +28,29 @@ public class FPSController : MonoBehaviour {
 		rotationX = Input.GetAxis("Mouse X") * sensitivity;
 		rotationY -= Input.GetAxis("Mouse Y") * sensitivity;
 		
+		//Mathf.Clamp limits the rotationY to between -70 and 70 degrees.
 		rotationY = Mathf.Clamp(rotationY, -70f, 70f);
 		
-		Vector3 movement = new Vector3(moveLeftRight, 0, moveForwardBack);
+		Vector3 movement = new Vector3(moveLeftRight, vertVelocity, moveForwardBack);
 		
+		//transform.rotate will change the rotation of the object this script is attatched to
 		transform.Rotate(0,rotationX,0);
-		camera.transform.localRotation = Quaternion.Euler(rotationY,0,0);
+		camera.transform.localRotation = Quaternion.Euler(rotationY,0,0); //Quaternion.Euler returns a rotation in the x,y and z axes.
 		
 		movement = transform.rotation * movement;		
 		player.Move(movement * Time.deltaTime);
+	}
+	
+	//FixedUpdate runs every second frame.
+	void FixedUpdate()
+	{
+		if(!player.isGrounded)
+		{
+			vertVelocity += Physics.gravity.y * Time.deltaTime; //Downward velocity is force of gravity * time
+		}
+		else
+		{
+			vertVelocity = 0f; //resets velocity when player lands
+		}
 	}
 }
