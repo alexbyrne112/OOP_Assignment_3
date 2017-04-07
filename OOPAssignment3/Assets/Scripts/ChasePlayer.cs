4a41f7935f670e95535650e62f513862e3b3f54f
c15492952 <alexwindows8music@gmail.com>
2017-04-07 10:26:23 +0100

    tried to get the enemy to follow the player but its not working yet

--- OOPAssignment3/Assets/Scripts/ChasePlayer.cs
+++ OOPAssignment3/Assets/Scripts/ChasePlayer.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChasePlayer : MonoBehaviour {
+public class ChasePlayer : MonoBehaviour 
+{
+	public LayerMask detectionLayer;
+	private Transform myTransform;
+	private UnityEngine.AI.NavMeshAgent myNavMeshAgent;
+	private Collider[] hitColliders;
+	private float checkRate;
+	private float nextCheck;
+	private float detectionRadius = 50;
 
-	// Use this for initialization
 	void Start () {
+		myTransform = transform;
+		myNavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		checkRate = Random.Range(0.8f,1.2f);
+	}
+	
+	void Update () 
+	{
+		CheckIfPLayerInRange();
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void CheckIfPLayerInRange()
+	{
+		if(Time.time > nextCheck && myNavMeshAgent.enabled == true)
+		{
+			nextCheck = Time.time + checkRate;
+				
+			hitColliders = Physics.OverlapSphere(myTransform.position, detectionRadius, detectionLayer);
+				
+			if(hitColliders.Length > 0)
+			{
+				myNavMeshAgent.SetDestination(hitColliders[0].transform.position);
+			}
+		}
 	}
 }
