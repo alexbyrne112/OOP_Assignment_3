57e3055f59ecad8b7abae93df901000bb1c00551
c15492952 <alexwindows8music@gmail.com>
2017-04-26 16:24:40 +0100

    last few changes

--- OOPAssignment3/Assets/Scripts/LoadingBarScript04.cs
+++ OOPAssignment3/Assets/Scripts/LoadingBarScript04.cs
@@ -18,8 +18,8 @@ public class LoadingBarScript04 : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Cursor.visible = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
